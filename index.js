1cc0689fb41b9536546648d902e8702bbac9d18e
Andrew Goodenough <ehgoodenough@gmail.com>
2023-10-04 06:47:25 +0900

    Added a new listen function.

--- index.js
+++ index.js
@@ -31,19 +31,22 @@ const Poin = {
         this.position.x = position.x
         this.position.y = position.y
     },
-}
+    "listen": function(element) {
+        if(element == undefined) return
 
-document.addEventListener("mousedown", (event) => Poin.setPressed(event.button, true))
-document.addEventListener("mouseup", (event) => Poin.setPressed(event.button, false))
-document.addEventListener("mousemove", (event) => Poin.setPosition({"x": event.clientX, "y": event.clientY}))
-document.addEventListener("pointerdown", (event) => Poin.setPressed(event.button || 0, true))
-document.addEventListener("pointerup", (event) => Poin.setPressed(event.button || 0, false))
-document.addEventListener("pointermove", (event) => Poin.setPosition({"x": event.clientX, "y": event.clientY}))
-document.addEventListener("touchstart", (event) => Poin.setPressed(0, true))
-document.addEventListener("touchend", (event) => Poin.setPressed(0, false))
-document.addEventListener("touchstart", (event) => Poin.setPosition({"x": event.touches[0].clientX, "y": event.touches[0].clientY}))
-document.addEventListener("touchend", (event) => Poin.setPosition({"x": event.touches[0].clientX, "y": event.touches[0].clientY}))
-document.addEventListener("touchmove", (event) => Poin.setPosition({"x": event.touches[0].clientX, "y": event.touches[0].clientY}))
+        element.addEventListener("mousedown", (event) => Poin.setPressed(event.button, true))
+        element.addEventListener("mouseup", (event) => Poin.setPressed(event.button, false))
+        element.addEventListener("mousemove", (event) => Poin.setPosition({"x": event.clientX, "y": event.clientY}))
+        element.addEventListener("pointerdown", (event) => Poin.setPressed(event.button || 0, true))
+        element.addEventListener("pointerup", (event) => Poin.setPressed(event.button || 0, false))
+        element.addEventListener("pointermove", (event) => Poin.setPosition({"x": event.clientX, "y": event.clientY}))
+        element.addEventListener("touchstart", (event) => Poin.setPressed(0, true))
+        element.addEventListener("touchend", (event) => Poin.setPressed(0, false))
+        element.addEventListener("touchstart", (event) => Poin.setPosition({"x": event.touches[0].clientX, "y": event.touches[0].clientY}))
+        element.addEventListener("touchend", (event) => Poin.setPosition({"x": event.touches[0].clientX, "y": event.touches[0].clientY}))
+        element.addEventListener("touchmove", (event) => Poin.setPosition({"x": event.touches[0].clientX, "y": event.touches[0].clientY}))
+    }
+}
 
 document.addEventListener("contextmenu", (event) => {
     if(Poin.isIgnoringContextMenu == true) {
